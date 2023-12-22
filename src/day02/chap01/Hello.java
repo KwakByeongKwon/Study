557dae2d0f7c29508ef506d26fffeb1dd2cf2327
byoungkwan <qudrnjs0428@naver.com>
2023-12-22 13:44:04 +0900

    Java study day02

---
+++ src/day02/chap01/Hello.java
@@ -0,0 +1,27 @@
+package day02.chap01;
+
+public class Hello {
+    public static void main(String[] args) {
+        char charTest = 'A';
+        int intTest = charTest;
+
+        System.out.println(charTest);
+        System.out.println(intTest);
+    }
+}
+
+////
+//// Source code recreated from a .class file by IntelliJ IDEA
+//// (powered by FernFlower decompiler)
+////
+//
+//package sec03.exam01;
+//
+//public class Hello {
+//    public Hello() {
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello, Java");
+//    }
+//}
