1fa54f9b514da1c7f003c1044e42309a8d07faf2
hgd <hgd@gmail.com>
2023-12-24 14:09:28 +0900

    폴더 수정

---
+++ day03/열거타입/_1_test.java
@@ -0,0 +1,9 @@
+package day03.열거타입;
+
+public class _1_test {
+    public static void main(String[] args) {
+        Week today = Week.SATURDAY;
+        Week birthday = null;  // 열거 타입 변수로 null값을 지정할 수 있음.
+        
+    }
+}
